8f2e4512e2cfac77b15c7adf8f9f09ece48739db
Eduardo Pasqualetti <49802002810@info.local>
2023-10-30 08:05:04 -0300

    .

--- React/hello/src/App.jsx
+++ React/hello/src/App.jsx
@@ -10,16 +10,11 @@ function App() {
       <Title nome="Eduardo" sobrenome="Pasqualetti" />
       <br /><br />
       <Container>
-        <CardEvento titulo="Iniciando React" texto="Breve descrição do evento, pode ser um paragrafo pequenoBreve descrição do evento,
-       pode ser um paragrafo pequeno.Breve descrição do evento, pode ser um paragrafo pequeno." textoLink="Participar" />
-        <CardEvento titulo="HTML CSS" texto="Breve descrição do evento, pode ser um paragrafo pequenoBreve descrição do evento,
-       pode ser um paragrafo pequeno.Breve descrição do evento, pode ser um paragrafo pequeno." textoLink="Participar" />
-        <CardEvento titulo="APi/JSON" texto="Breve descrição do evento, pode ser um paragrafo pequenoBreve descrição do evento,
-       pode ser um paragrafo pequeno.Breve descrição do evento, pode ser um paragrafo pequeno." textoLink="Participar" />
-        <CardEvento titulo="JavaScript" texto="Breve descrição do evento, pode ser um paragrafo pequenoBreve descrição do evento,
-       pode ser um paragrafo pequeno.Breve descrição do evento, pode ser um paragrafo pequeno." textoLink="Participar" />
-        <CardEvento titulo="SqlServer" texto="Breve descrição do evento, pode ser um paragrafo pequenoBreve descrição do evento,
-       pode ser um paragrafo pequeno.Breve descrição do evento, pode ser um paragrafo pequeno." textoLink="Participar" />
+        <CardEvento titulo="Iniciando React" texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dicta veritatis," textoLink="Participar" />
+        <CardEvento titulo="HTML CSS" texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dicta veritatis," textoLink="Participar" />
+        <CardEvento titulo="APi/JSON" texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dicta veritatis," textoLink="Participar" />
+        <CardEvento titulo="JavaScript" texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dicta veritatis," textoLink="Participar" />
+        <CardEvento titulo="SqlServer" texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dicta veritatis," textoLink="Participar" />
       </Container>
     </div>
   );
